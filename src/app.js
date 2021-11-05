46814bca36f738b885752ae0fde993f9d95d592a
Castilloo <delcaos12@gmail.com>
2021-11-04 23:17:37 -0500

    Agregando sistema de carpetas para MVC, EJS, agregando controlers basicos, y modificando URL's

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const path = require('path');
 const color = require('colors');
-const routes = require('./routes/index.js');
+const routes = require('./routes/index.routes.js');
 
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, './public')));
+app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname, 'views'));
+
+app.use(express.static(path.resolve(__dirname, '../public')));
+// app.use(express.static(path.resolve(__dirname, '..', 'public'))); 
+app.set('view engine', 'ejs');
+
 app.use('/', routes);
 app.listen(port, () => console.log(`Starting in port ${port}`.bold.magenta));
---
+++ src/controllers/main.controller.js
@@ -0,0 +1,19 @@
+const controller = {
+    getIndex : function(req, res) {
+        res.render('index');
+    },
+    getProductDetail : function(req, res) {
+        res.render('./products/product-detail');
+    },
+    getCart : function(req, res) {
+        res.render('./products/cart');
+    },
+    getSignIn : function(req, res) {
+        res.render('./users/sign-in');
+    },
+    getLogin : function(req, res) {
+        res.render('./users/login');
+    }
+}
+
+module.exports = controller;
